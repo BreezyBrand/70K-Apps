65406dc1252c7daff1179b2255a5cad6973a9960
BreezyBrand <brandonjamessorensen@gmail.com>
2025-03-07 13:01:32 -0500

    Initial Commit

---
+++ 70KApps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _70KApps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
